2d06164e65a9f9618dfc34bea03951369bf86172
lts-pj <lliamsymonds04@gmail.com>
2025-02-16 09:52:43 +1000

    added toggle button

---
+++ src/components/ToggleButton.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { motion } from "motion/react"
+
+interface ToggleButtonProps {
+    backgroundColour: string,
+    activeColour: string,
+    toggle: boolean,
+    setToggle: React.Dispatch<React.SetStateAction<boolean>>,
+    option1: string,
+    option2: string
+}
+
+function ToggleButton({backgroundColour, activeColour, toggle, setToggle, option1, option2}: ToggleButtonProps ) {
+    return (<div className={`w-full h-12 relative rounded-2xl bg-[${backgroundColour}]`}>
+        <button 
+            className="w-full h-full absolute cursor-pointer z-50"
+            onClick={() => setToggle(!toggle)}
+        />
+        <motion.div className={`absolute h-full w-1/2 rounded-2xl bg-[${activeColour}]`} animate={{x: toggle ? "100%" : 0}}/>
+        <p className="text-2xl text-white font-sans w-1/2 text-center absolute top-1/2 -translate-y-1/2">{option1}</p>
+        <p className="text-2xl text-white font-sans w-1/2 text-center absolute left-1/2 top-1/2 -translate-y-1/2">{option2}</p>
+    </div>)
+}
+
+export default ToggleButton;
